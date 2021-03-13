b8c23030457bae62e1c23f997014386a72da3995
Boggot <alexweaver1982@gmail.com>
2021-03-13 19:29:14 +0000

    WIP navbar

--- WebApp/ClientApp/src/__tests__/components/NavBar.test.js
+++ WebApp/ClientApp/src/__tests__/components/NavBar.test.js
@@ -1,6 +1,12 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import NavBar from '../../components/NavBar.js';
 
 test('it displays AppBar', () => {
     render(<NavBar />);
 });
+
+test('it displays Game-Board inside navbar', () => {
+    render(<NavBar />);
+    const title = screen.getByText('Game-Board');
+    expect(title).toBeInTheDocument();
+})
--- WebApp/ClientApp/src/components/NavBar.js
+++ WebApp/ClientApp/src/components/NavBar.js
@@ -2,7 +2,11 @@ import * as React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 
 function NavBar() {
-    return <AppBar position="static"></AppBar>;
+    return <AppBar position="static">
+            <Typography variant="h6" className={classes.title}>
+      Game-Board
+    </Typography>
+    </AppBar>;
 }
 
 export default NavBar;
