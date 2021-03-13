1b496821ca6908f454c47ef3f1ff2415da9d6be4
Boggot <alexweaver1982@gmail.com>
2021-03-13 18:12:18 +0000

    Initial code cleanup

--- WebApp/ClientApp/src/__tests__/App.test.js
+++ WebApp/ClientApp/src/__tests__/App.test.js
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
 
 test('renders welcome button', () => {
   render(<App />);
   const button = screen.getByText('Welcome');
   expect(button).toBeInTheDocument();
 });
+
+test('displays Game-Board header', () => {
+  render(<App />);
+  const header = screen.getByText('Game-Board');
+  expect(header).toBeInTheDocument();
+})
